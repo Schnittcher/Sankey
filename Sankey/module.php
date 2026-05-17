614f4a1e70d087896b0e608f0ab0ea8bf17c6d27
mb-stern <95777848+mb-stern@users.noreply.github.com>
2026-05-17 18:58:58 +0200

    Farrbänderung wieder rückgängig

--- Sankey/module.php
+++ Sankey/module.php
@@ -146,14 +146,8 @@ class Sankey extends IPSModule
                     $nodeColorMap[$source] = $hex;
                 }
 
-                $color = intval($link['Color'] ?? 0);
-
-                if ($color > 0) {
-                    $hex = sprintf('#%06x', $color);
-
-                    if (!array_key_exists($source, $nodeColorMap)) {
-                        $nodeColorMap[$source] = $hex;
-                    }
+                if (!array_key_exists($target, $nodeColorMap)) {
+                    $nodeColorMap[$target] = $hex;
                 }
             }
         }
